2d90216541e6e63864e321ad365d6b6a127a7828
Michael Fatemi <myfatemi04@gmail.com>
2022-03-28 14:42:32 -0400

    threaded downloads

--- corporation_search_api/extract_companies.py
+++ corporation_search_api/extract_companies.py
@@ -1,3 +1,5 @@
+import threading
+from collections import deque
 import bs4
 import requests
 import os
@@ -16,6 +18,46 @@ for option in soup.find_all('option'):
         pairings.append((short_name, long_name))
 
 print(len(pairings))
+
+
+queue = deque(pairings)
+num_completed = 0
+
+
+def thread_loop():
+    global num_completed
+
+    while queue:
+        short_name, long_name = queue.popleft()
+        filename = f"./data/{short_name}.csv"
+
+        if os.path.exists(filename):
+            num_completed += 1
+            continue
+
+        csv_response = requests.get(
+            f'https://violationtracker.goodjobsfirst.org/prog.php?parent={short_name}&detail=csv_results', headers={
+                'User-Agent': 'Chrome'
+            })
+
+        text = csv_response.text
+        with open(filename, "w") as f:
+            f.write(text)
+
+        num_completed += 1
+        print(f"{num_completed} / {len(pairings)} - {long_name}, {len(text)=}...")
+
+
+n_threads = 25
+threads = [threading.Thread(target=thread_loop) for _ in range(n_threads)]
+
+for thread in threads:
+    thread.start()
+
+for thread in threads:
+    thread.join()
+
+exit()
 
 # https://violationtracker.goodjobsfirst.org/prog.php?parent=amazoncom&detail=csv_results
 for short_name, long_name in pairings:
