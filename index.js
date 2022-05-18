ab3aac1340ac860e1916ff8829f45fd63f2a98d4
Michael Fatemi <myfatemi04@gmail.com>
2022-05-18 10:50:18 -0400

    add ua

--- index.js
+++ index.js
@@ -38,7 +38,12 @@ async function parseCSVNetwork(url) {
   }
 
   return new Promise((resolve, reject) => {
-    fetch(url)
+    fetch(url, {
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36",
+      },
+    })
       .then((response) => {
         if (response.status != 200) {
           csvCache[url] = null;
